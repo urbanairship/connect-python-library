11b33eb2b18ea3f8766c34d97a1361046a0b418e
Adam Lowry <adam@urbanairship.com>
2015-11-16 11:42:44 -0800

    Update connection tests with new class/method args

--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -12,16 +12,16 @@ from uaconnect import consumer
 class TestConsumer(unittest.TestCase):
 
     def test_connect_success(self):
-        c = consumer.Connection('key', 'url')
+        c = consumer.Connection('key', 'token', 'url')
 
         conn = mock.Mock()
         conn.status_code = 200
 
         with mock.patch('uaconnect.consumer.requests.post', return_value=conn):
-            c.connect('token', None)
+            c.connect(None)
 
     def test_connect_307(self):
-        c = consumer.Connection('key', 'url')
+        c = consumer.Connection('key', 'token', 'url')
         c._headers = mock.Mock(return_value={'auth': 'fake'})
 
         redir_conn = mock.Mock()
@@ -37,7 +37,7 @@ class TestConsumer(unittest.TestCase):
         # First connection returns a 307 with cookies, second returns a 200
         with mock.patch('uaconnect.consumer.requests.post',
                 side_effect=[redir_conn, good_conn]) as post:
-            c.connect('token', None)
+            c.connect(None)
 
             # Ensure no cookies the first time, then send cookies returned in
             # the 307
@@ -49,7 +49,7 @@ class TestConsumer(unittest.TestCase):
             ])
 
     def test_connect_failure_retry(self):
-        c = consumer.Connection('key', 'url')
+        c = consumer.Connection('key', 'token', 'url')
 
         conn = mock.Mock()
         conn.status_code = 200
@@ -57,17 +57,17 @@ class TestConsumer(unittest.TestCase):
         with mock.patch('uaconnect.consumer.time.sleep') as sleep:
             with mock.patch('uaconnect.consumer.requests.post',
                     side_effect=[consumer.requests.exceptions.ConnectionError(), conn]):
-                c.connect('token', None)
+                c.connect(None)
 
                 sleep.assert_called_with(0.1)
 
     def test_connect_failure_retries_exceeded(self):
-        c = consumer.Connection('key', 'url')
+        c = consumer.Connection('key', 'token', 'url')
 
         with mock.patch('uaconnect.consumer.time.sleep') as sleep:
             with mock.patch('uaconnect.consumer.requests.post',
                     side_effect=consumer.requests.exceptions.ConnectionError()):
-                self.assertRaises(consumer.ConnectionError, c.connect, 'token', None)
+                self.assertRaises(consumer.ConnectionError, c.connect, None)
 
                 # Ensure while reconnecting we got to the max backoff
                 sleep.assert_called_with(10)
